20d4008a2dd7991383ebd340d9161d193e9097ab
Jan Fooken <git@bode.fun>
2023-05-12 12:36:41 +0200

    docs: Add a TODO

--- cmd/add.go
+++ cmd/add.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// TODO: Make this into a function which receives an app instance like https://github.com/pocketbase/pocketbase/blob/c6d599244239ed17b2f2f7ce892b1279ddabf5ac/cmd/serve.go#L27
 var AddCmd = &cobra.Command{
 	Use:     "add",
 	Aliases: []string{"a"},
