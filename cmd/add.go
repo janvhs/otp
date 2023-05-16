ee25352faea9b1bc2ecea14d0cad5779c3351530
Jan Fooken <git@bode.fun>
2023-05-16 17:07:40 +0200

    feat: wip add add command

--- cmd/add.go
+++ cmd/add.go
@@ -8,12 +8,22 @@ import (
 )
 
 func NewAddCommand(app core.App) *cobra.Command {
-	return &cobra.Command{
+	command := &cobra.Command{
 		Use:     "add",
 		Aliases: []string{"a"},
 		Short:   "Add a new account to your collection",
+		Args:    cobra.MatchAll(cobra.ExactArgs(2)),
+		ArgAliases: []string{
+			"key",
+			"base32-secret",
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return fmt.Errorf("add is not implemented yet")
 		},
 	}
+
+	command.Flags().UintP("digits", "d", 6, `The amount of digits your code should have.
+You can pick between 6, 7 or 8 digits.`)
+
+	return command
 }
